09946cf9fb00f8f3daa51cdae9717ed32fc353d6
more-on-the-way <obonbonstore@gmail.com>
2025-10-11 21:39:43 -0500

    Remove duplicate index.css import
    Removed duplicate import of index.css.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import StudentLoanPayoff from './StudentLoanPayoff';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
